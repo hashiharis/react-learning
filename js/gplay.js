9c339d7a96de82bd7b4207ab96ec09427244bbcb
hashiharis <100787257+hashiharis@users.noreply.github.com>
2024-07-23 14:41:31 +0530

    clonegplay

---
+++ js/gplay.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll',function(){
+    const navbar=document.querySelector("nav");
+    if(window.scrollY>0){
+        navbar.classList.add('sticky');
+    }
+    else{
+        navbar.classList.remove('sticky');
+    }
+})
