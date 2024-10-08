79b5118879c57c51facfca19c119c856da4725aa
hashiharis <100787257+hashiharis@users.noreply.github.com>
2024-10-08 13:41:58 +0530

    express-commit

--- express-routing/routes/user.routes.js
+++ express-routing/routes/user.routes.js
@@ -1,149 +1,176 @@
-const express=require("express");
-const userRouter=express.Router();
+const express = require("express");
+const userRouter = express.Router();
 
 // userRouter.get('/new',(req,res)=>{
 //     res.send("New user router")
 // })
 
-let userDB=[];
+let userDB = [];
 
+userRouter.post("/signup", (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
+    if (!name|| !email|| !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
 
-userRouter.post('/signup',(req,res)=>{
-    try{
+    const isEmailTaken = userDB.find((user) => user.email === email);
 
-    const {name,email,password}=req.body;
+    if (isEmailTaken) {
+      return res.status(400).json({ message: "Email already exist" });
+    }
 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid entry" });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 8 characters long" });
+    }
+
+    const userData = { id: userDB.length + 1, name, email, password };
+    userDB.push(userData);
+    return res
+      .status(201)
+      .json({ message: "Registration successful", data: userDB });
+  } catch (error) {
+    console.log("Signup error", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+userRouter.post("/signin", (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email||!password) {
+      return res
+        .status(400)
+        .json({ message: "Please enter email and password" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid entry" });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 8 characters long" });
+    }
+
+    const userFound = userDB.find((user) => user.email === email);
+
+    if (!userFound) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email id and password" });
+    }
+
+    if (userFound.password !== password) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email id and password" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Login successful", user: userFound });
+  } catch (error) {
+    console.log("Sign In error", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+userRouter.get("/", (req, res) => {
+  try {
+    return res.status(200).json({ message: "User details", users: userDB });
+  } catch (error) {
+    console.log("fetch error", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+userRouter.get("/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = userDB.find((user) => user.id == id);
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    return res.status(200).json({ message: "User Found", data: user });
+  } catch (error) {
+    console.log("fetch error", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+userRouter.patch("/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const{name,email,password}=req.body;
+    const userFound = userDB.find((user) => user.id == id);
+    if (!userFound) {
+      return res.status(400).json({ message: "User not found" });
+    }
     
-    if(!name,!email,!password){
-        return res.status(400).json({message:"All fields are required"})
-    }
-
-    const isEmailTaken=userDB.find((user)=>user.email===email)
-
-    if(isEmailTaken){
-        return res.status(400).json({message:"Email already exist"})
-    }
-   
-    const emailRegex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(!emailRegex.test(email)){
-        return res.status(400).json({message:"Invalid entry"})
-    }
-
-    if(password.length<8){
-        return res.status(400).json({message:"Password must be atleast 8 characters long"})
+    if(!name && !email && !password){
+        return res.status(400).json({message:"user details not found"})
     }
     
-
-    const userData={id:userDB.length+1,name,email,password}
-    userDB.push(userData)
-    return res.status(201).json({message:"Registration successful",data:userDB})
-
-    }catch(error){
-        console.log("Signup error",error)
-        return res.status(500).json({message:"Server error"});
+    if(name){
+        if(userFound.name!==name){
+            userFound.name=name;
+        }
     }
-})
-
-userRouter.post('/signin',(req,res)=>{
-    try{
-
-        const{email,password}=req.body
-
-        if(!email,!password){
-            return res.status(400).json({message:"Please enter email and password"})
+    if(email){
+        if(userFound.email!==email){
+            userFound.email=email;
         }
-
-        const emailRegex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!emailRegex.test(email)){
-            return res.status(400).json({message:"Invalid entry"})
-        }
-
-        if(password.length<8){
-            return res.status(400).json({message:"Password must be atleast 8 characters long"})
-        }
-
-        const userFound=userDB.find((user)=>user.email===email);
-
-        if(!userFound){
-            return res.status(400).json({message:"Incorrect email id and password"})
-        }
-
+    }
+    if(password){
         if(userFound.password!==password){
-            return res.status(400).json({message:"Incorrect email id and password"})
+            userFound.password=password;
         }
-
-        return res.status(200).json({message:"Login successful",user:userFound})
     }
-    
-    catch(error){
-      console.log("Sign In error",error)
-      return res.status(500).json({message:"Server error"})
+
+    // if (userFound.password !== newPassword) {
+    //   userFound.password = newPassword;
+    // } else {
+    //   return res
+    //     .status(400)
+    //     .json({ message: "Same as old password, please try a new one" });
+    // }
+
+    return res
+      .status(200)
+      .json({ message: "password updated", updated: userDB });
+  } catch (error) {
+    console.log("updation error", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+userRouter.delete("/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const userFound = userDB.find((user) => user.id == id);
+    if (!userFound) {
+      return res.status(404).json({ message: "User not found" });
     }
-})
-
-userRouter.get('/',(req,res)=>{
- try{
-     
-    return res.status(200).json({message:"User details", users:userDB})
- }catch(error){
-    console.log("fetch error",error)
-    return res.status(500).json({message:"Server error"})
- }
-})
-
-userRouter.get('/:id',(req,res)=>{
-    try{
-       
-        const {id}=req.params
-        const user=userDB.find((user)=>user.id==id)
-        if(!user){
-            return res.status(400).json({message:"User not found"})
-        }
-        return res.status(200).json({message:"User Found",data:user})
-    }catch(error){
-        console.log("fetch error",error)
-    return res.status(500).json({message:"Server error"}) 
-    }
-})
-
-userRouter.patch('/:id',(req,res)=>{
-    try{
-     const {id}=req.params
-     const userFound=userDB.find((user)=>user.id==id)
-     if(!userFound){
-        return res.status(400).json({message:"User not found"})
-     }
-     let newPassword="567891234"
-     if(userFound.password!==newPassword){
-        userFound.password=newPassword
-     }
-     else{
-        return res.status(400).json({message:"Same as old password, please try a new one"})
-     }
-    
-    return res.status(200).json({message:"password updated",updated:userDB})
-
-    }catch(error){
-        console.log("updation error",error)
-        return res.status(500).json({message:"Server error"}) 
-    }
-})
-
-userRouter.delete('/:id',(req,res)=>{
-    try{
- 
-         const {id}=req.params
-         const userFound=userDB.find((user)=>user.id==id)
-         if(!userFound){
-            return res.status(404).json({message:"User not found"})
-         }
-         userDB=userDB.filter((user)=>user.id!==parseInt(id))
-         console.log("userDB=>",userDB)
-         return res.status(200).json({message:"User deleted successfully", data:userDB})
-    }catch(error){
-        console.log("deletion error",error)
-        return res.status(500).json({message:"Server error"}) 
-    }
-})
-module.exports=userRouter;
+    userDB = userDB.filter((user) => user.id !== parseInt(id));
+    console.log("userDB=>", userDB);
+    return res
+      .status(200)
+      .json({ message: "User deleted successfully", data: userDB });
+  } catch (error) {
+    console.log("deletion error", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+module.exports = userRouter;
